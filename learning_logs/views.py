7a76a20aedd08884148856e67911fe9bb5fb0a2b
bonkaj <bonkers125@gmail.com>
2020-05-28 09:27:02 -0500

    "created base template"

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     """The home page for Learning Log."""
-    return render(request, 'learning_logs/index.html')
+    return render(request, '/Users/bonkers/PycharmProjects/PythonWebProject/learning_logs/templates/index.html')
 
